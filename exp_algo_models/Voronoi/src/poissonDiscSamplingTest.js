3e609414b42d1e141d2a5d2bdc9ba848c2dff701
ScarlettRed6 <jcaamrelo@gmail.com>
2026-03-18 15:28:37 +0800

    Added triangulation in poissonDiscSamplingTest file, New file for later implementation with a library

--- exp_algo_models/Voronoi/src/poissonDiscSamplingTest.js
+++ exp_algo_models/Voronoi/src/poissonDiscSamplingTest.js
@@ -1,6 +1,4 @@
-
 let numPoints = 0;
-const points = poissonDiskSampling(16, 16, 1);
 
 let canvas = document.getElementById("map");
 let ctx = canvas.getContext('2d');
@@ -13,6 +11,17 @@ function randomHSL() {
     return `hsl(${HUE}, ${SATURATION}%, ${LIGHTNESS}%)`;
 }
 
+function randomPoints(width, height, k = 100){
+    let points = [];
+    for(let i = 0; i < k; i++){
+        points.push({
+            x: Math.random() * width,
+            y: Math.random() * height
+        });
+        numPoints++;
+    }
+    return points;
+}
 
 function poissonDiskSampling(width, height, radius, k = 30) {
 
@@ -65,10 +74,13 @@ function poissonDiskSampling(width, height, radius, k = 30) {
         x: Math.random() * width,
         y: Math.random() * height
     });
-
+    
     while (active.length > 0) {
 
+        console.log(`Active points: ${active.length}`);
         const index = Math.floor(Math.random() * active.length);
+        console.log(`Index from active points: ${index}`);
+        
         const point = active[index];
 
         let found = false;
@@ -100,17 +112,79 @@ function poissonDiskSampling(width, height, radius, k = 30) {
             active.splice(index, 1);
         }
     }
-
+    console.log(`Active points after loop: ${active.length}`);
     return points;
 }
+
+const points = poissonDiskSampling(16, 16, 1, 30);
+/* const points = randomPoints(10, 10, 3); */
+const delaunay = Delaunator.from(points, loc => loc.x, loc => loc.y);
 
 ctx.save();
 ctx.scale(canvas.width / 16, canvas.height / 16);
 ctx.fillStyle = randomHSL();
-for (let {x, y} of points) {
+for (let point of points) {
+    const {x, y} = point;
     ctx.beginPath();
     ctx.arc(x, y, 0.1, 0, 2 * Math.PI);
     ctx.fill();
 }
 ctx.restore();
 console.log(`NUMBER OF POINTS ${numPoints}`);
+
+
+//TRYING TO IMPLEMENT THE TRIANGULATION
+
+function calculateCentroids(points, delaunay) {
+    const numTriangles = delaunay.halfedges.length / 3;
+    let centroids = [];
+    for (let t = 0; t < numTriangles; t++){
+        let sumOfX = 0, sumOfY = 0;
+        for (let i = 0; i < 3; i++){
+            let s = 3*t + i;
+            let p = points[delaunay.triangles[s]];
+            sumOfX += p.x; 
+            sumOfY += p.y;
+        }
+        centroids[t] = {x: sumOfX / 3, y: sumOfY / 3};
+    }//End of outer loop
+    return centroids;
+}
+
+function triangleOfEdge(e) {
+    return Math.floor(e / 3);
+}
+
+let map = {
+    points,
+    numRegions: points.length,
+    numTriangles: delaunay.halfedges.length / 3,
+    numEdges: delaunay.halfedges.length,
+    halfedges: delaunay.halfedges,
+    triangles: delaunay.triangles,
+    centers: calculateCentroids(points, delaunay),
+};
+
+function drawCellBoundaries(canvas, map) {
+    let { points, centers, halfedges, triangles, numEdges} = map;
+    let ctx = canvas.getContext('2d');
+    ctx.save();
+    ctx.scale(canvas.width / 14, canvas.height / 14);
+    ctx.lineWidth = 0.02;
+    ctx.strokeStyle = "black";
+    for (let e = 0; e < numEdges; e++) {
+        if (e < halfedges[e]) {
+            const p = centers[triangleOfEdge(e)];
+            const q = centers[triangleOfEdge(halfedges[e])];
+            ctx.beginPath();
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(q.x, q.y);
+            ctx.stroke();
+        }
+    }
+    ctx.restore();
+}
+
+drawCellBoundaries(canvas, map);
+
+
